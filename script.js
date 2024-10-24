1799ba0cad41942d804c73b7fd48fad4f7c1fada
Ding Zhengtian <dingzhengtiansasc@163.com>
2024-10-24 02:45:09 +0200

    changes

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+// 向右滑动
+function scrollLef() {
+    const container = document.querySelector('.experience-details-wrapper');
+    container.scrollBy({
+        left: -300, // 这个值根据你的项目容器的宽度来设置
+        behavior: 'smooth'
+    });
+}
+
+function scrollRig() {
+    const container = document.querySelector('.experience-details-wrapper');
+    container.scrollBy({
+        left: 300, // 这个值根据你的项目容器的宽度来设置
+        behavior: 'smooth'
+    });
+}
